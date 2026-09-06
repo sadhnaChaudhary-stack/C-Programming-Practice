03db9cf9599fed3875a97a4d58969107430b7666
sadhnaChaudhary-stack <sadhna2403chaudhary2005@gmail.com>
2026-09-06 19:43:00 +0530

    01 to 07 questions in 01-basic-for-loop.c

---
+++ 02-Loop/01-Basic-Loop-Questions/01-Basic-for-loop.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+void main()
+{
+
+  // Q1.Print numbers from 1 to 10.
+
+  /*for(int i=1 ; i<=10 ; i++){
+      printf("\ni : %d",i);
+  }*/
+
+  // Q.2 Print numbers from 10 to 1.
+
+  /*for(int i=10 ; i>=1 ; i--){
+    printf("\ni : %d",i);
+  }*/
+
+  // Q.3 Print numbers from 1 to 100.
+
+  /*for(int i=1 ; i<=100 ; i++){
+    printf("\ni = %d",i);
+  }*/
+
+  // Q.4 Print all even numbers from 1 to 100.
+
+  /*for(int i=1 ;  i<=100 ; i++){
+    if(i%2==0){
+      printf("\ni : %d",i);
+    }
+  }*/
+
+  // Q.5 Print all odd numbers from 1 to 100.
+
+  /*for(int i=1 ; i<=100 ; i++){
+    if(i%2!=0){
+      printf("\ni : %d",i);
+    }
+  }*/
+
+  // Q.6 Print numbers between two given numbers.
+  // Suppose the user enters :
+
+  //     Start = 5 End = 10
+
+  //     Output :
+
+      
+  //     5 6 7 8 9 10 
+      
+      
+     /* int a , b ;
+      printf("Enter Starting Value : ");
+      scanf("%d",&a);
+
+      printf("Enter Ending Value : ");
+      scanf("%d",&b);
+
+      for(int i=a ; i<=b ; i++){
+        printf("\n %d",i);
+      }*/
+
+
+      // Q.7 Print the first n natural numbers.
+      
+}
