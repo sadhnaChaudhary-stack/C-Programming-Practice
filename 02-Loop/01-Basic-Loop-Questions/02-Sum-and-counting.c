2e5fafb6fbd35ae17c10fe0a722a6de90eb34322
sadhnaChaudhary-stack <sadhna2403chaudhary2005@gmail.com>
2026-09-06 20:22:50 +0530

    15 questions of sum and counting

---
+++ 02-Loop/01-Basic-Loop-Questions/02-Sum-and-counting.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+int main() {
+
+    // Q10. Find the sum of numbers from 1 to 10.
+
+    /*int sum = 0;
+
+    for(int i = 1; i <= 10; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum = %d", sum);*/
+
+    // Q.11 Find the sum of the first n natural numbers.
+
+    /*int n ,i ,sum =0; 
+    printf("Enter the value for n : ");
+    scanf("%d",&n);
+
+    for(i=1 ; i<=n ; i++){
+      sum += i;
+    }
+
+    printf("Sum : %d", sum);*/
+
+    // Q.12 Find the sum of all even numbers from 1 to n.
+
+    /*int n , i , sum=0;
+
+    printf("Enter Value of n : ");
+    scanf("%d",&n);
+
+    for(i=1 ; i<=n ; i++){
+      if(i%2==0){
+        sum = sum + i;
+      }
+    }
+    printf("Sum : %d",sum);*/
+
+    // Q.13 Find the sum of all odd numbers from 1 to n.
+
+    /*int n , i, sum=0;
+
+    printf("Enter the Value of n : ");
+    scanf("%d",&n);
+
+    for(i=1 ; i<=n ; i++){
+      if(i%2!=0){
+        sum += i;
+      }
+    }
+
+    printf("sum : %d",sum);*/
+
+    // Q.14 Count how many numbers are divisible by 5 between 1 and 100.
+
+    /*int i,count=0;
+
+    for(i=1 ; i<=100 ; i++){
+      if(i%5==0){
+        count += 1;
+      }
+    }
+
+    printf("%d ",count);*/
+    
+
+    // Q.15 Count how many even and odd numbers are present between 1 and n.
+
+    /*int even = 0 , odd = 0 , i , n;
+
+    printf("Enter the Value of n : ");
+    scanf("%d",&n);
+
+    for(i=1; i<=n ; i++){
+      if(i%2==0){
+        even+=1;
+      }else{
+        odd+=1;
+      }
+    }
+
+    printf("\n Even : %d ", even);
+    printf("\n odd : %d", odd);*/
+    
+
+    
+
+
+    return 0;
+}
