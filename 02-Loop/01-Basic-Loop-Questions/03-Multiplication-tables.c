b82531a9c6e37d2244d677f4c154c88e878d3d90
sadhnaChaudhary-stack <sadhna2403chaudhary2005@gmail.com>
2026-09-06 20:50:38 +0530

    multiplication question

---
+++ 02-Loop/01-Basic-Loop-Questions/03-Multiplication-tables.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main(){
+
+  // Q.16 Print the multiplication table of a given number.
+  /*int n , i ;
+  printf("Enter a number : ");
+  scanf("%d",&n);
+
+  for(i=1 ; i<=10 ; i++){
+    printf("\n%d * %d = %d",n , i , n*i);
+  }*/
+
+  // Q.17 Print multiplication tables from 1 to 10.
+  /*for(int i=1 ; i<=50 ; i++){
+    for(int j=1 ; j<=50 ; j++){
+      printf("\t%d",i*j);
+    }
+    printf("\n");
+  }*/
+
+  // Q.18 Print a multiplication table in reverse order.
+
+  /*int n ;
+
+  printf("Enter the Value for n : ");
+  scanf("%d",&n);
+
+  for(int i=10 ; i>=1 ; i--){
+    printf("\n%d * %d = %d",n , i , n*i); 
+  }*/
+
+  // Q.19 Print tables from a given starting number to ending number.
+  /*int start, end , i , j;
+  printf("Enter a starting number : ");
+  scanf("%d",&start);
+
+  printf("Enter a End number : ");
+  scanf("%d",&end);
+
+  for(i=start ; i<=end ; i++){
+    for(j=1 ; j<=10 ; j++){
+      printf("\n%d * %d = %d",i , j , i*j);
+    }
+    printf("\n");
+  }*/
+
+  return 0;
+}
